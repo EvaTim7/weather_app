285bf174a7236620e54dcdb2362c2f4739f1688d
vibintim <vibinmky@gmail.com>
2024-12-12 12:41:03 +0530

    updatedServerv1

--- server.py
+++ server.py
@@ -31,5 +31,5 @@ def get_weather():
 
 
 if __name__ == "__main__":
-    #serve(app,debug=True, host="0.0.0.0", port=8000)
-    app.run(debug=True, host="0.0.0.0", port=8000)
+    serve(app,debug=True, host="0.0.0.0", port=8000)
+    #app.run(debug=True, host="0.0.0.0", port=8000)
